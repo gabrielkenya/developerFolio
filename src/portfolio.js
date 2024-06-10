e361b8a7693ff17733ccee82646a8c63fa8a8bbf
Eugenius <103780583+gabrielkenya@users.noreply.github.com>
2024-06-10 14:34:32 +0300

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -127,7 +127,7 @@ const educationInfo = {
   schools: [
     {
       schoolName: "Jomo Kenyatta University of Agricukture and Technology",
-      logo: require("./assets/images/jkuat.png"),
+      logo: require("./assets/images/jkuat.jpeg"),
       subHeader: "Bachelor Degree in Mathematics and Computer Science",
       duration: "September 2018 - Present",
       ]
